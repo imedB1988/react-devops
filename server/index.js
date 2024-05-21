65b45ed2f348020aaf8049addbe01987bb839d01
imedB1988 <imededdine.benzarti@gmail.com>
2024-05-21 23:06:47 +0100

    first commit home video 1:22:10

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8080
 const schemaData = mongoose.Schema({
     name : String,
     email: String,
-    phone: Number
+    phone: String
 }, {
     timestamps: true
 })
